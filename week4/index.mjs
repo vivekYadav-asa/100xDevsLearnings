4e480b59496f1aebaf3e6a5cbae2871d8a13148a
vivekYadav-asa <vy6751536@gmail.com>
2025-03-26 01:31:19 +0530

    added a changes in chalk node module

--- week4/index.mjs
+++ week4/index.mjs
@@ -1,4 +1,5 @@
 //learning about nodejs.
 
 import chalk from "chalk";
-console.log(chalk.redBright.line("hello world"));
+
+console.log(chalk.redBright.underline("hello world"));  //^ carrot the scurity issue arriese while using by multi person
