62a0f3e00b93c2f8396de7953ae6f4506b9b605b
vivekYadav-asa <vy6751536@gmail.com>
2025-03-26 19:28:30 +0530

    comment the unwanted things in code

--- week4/index.js
+++ week4/index.js
@@ -1,4 +1,3 @@
-//as-1create a commmand line interface that lets the user spacify a file path andthe nodejs processs counts the number of word inside it
 //  const thenable={
 //     then:function(onFulfilled){
 //         setTimeout(()=>onFulfilled(42),1000);
@@ -9,16 +8,18 @@
 //     console.log("hi there");
 //   }
 //   main();
-const fs= require("fs")
-function main(fileName){//process argv gives the file name we give it node 
-    fs.readFile(fileName,"utf-8",function(err,data){
-        let total=0;
-        for(let i=0;i<data.length;i++){
-            if(data[i] == ' '){
-                total++;
-            }
-        }
-        console.log(total +1);
-    });
-} 
-main('a.txt');
+// const fs= require("fs")
+// function main(fileName){//process argv gives the file name we give it node 
+//     fs.readFile(fileName,"utf-8",function(err,data){
+//         let total=0;
+//         data=data.trim();
+//         for(let i=0;i<data.length;i++){
+//             if(data[i]===' '){
+//                 total++;
+//             }
+//         }
+//         console.log(total);
+//     });
+// } 
+// main('a.txt');
+//as-1create a commmand line interface that lets the user spacify a file path andthe nodejs processs counts the number of word inside it
