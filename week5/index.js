1e559333012e2feffa01af792ba8ed86de95bd3b
vivekYadav-asa <vy6751536@gmail.com>
2025-04-05 23:29:28 +0530

    learning headers

---
+++ week5/index.js
@@ -0,0 +1,2 @@
+//headers->http headers are the key value pairs that are sent between a client and a server in an http req and response.they convey metadata
+//  about the request and response such as content type and auth information etc.
