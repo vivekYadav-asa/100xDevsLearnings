24867d9d99efbfa73b617e7a84a25e00fe8b6528
vivekYadav-asa <vy6751536@gmail.com>
2025-04-07 18:51:36 +0530

    middleware implementation

--- week5/index.js
+++ week5/index.js
@@ -2,12 +2,27 @@
 //  about the request and response such as content type and auth information etc.
 const express=require('express')
 const app=express();
-app.get('/add/:a/:b',function(req,res){
+let requestCount =0;
+function requestIncrease(){
+    requestCount=requestCount+1;
+    console.log("total number of request =" + requestCount);
+    
+}
+// better routing ,add database,middleware
+app.get('/add',function(req,res){
+    requestIncrease()
     const a=parseInt(req.params.a);
     const b=parseInt(req.params.b);
     res.json({
        answer:a+b
     })
 })
+app.get('/multiply',function(req,res){
+    requestIncrease();
+    const a=parseInt(req.params.a);
+    const b=parseInt(req.params.b);
+    res.json({
+       answer:a*b
+    })
+})
 app.listen(3000)
-// better routing ,add database,middleware
