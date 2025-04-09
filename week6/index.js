df69e0f750b988ba235963f11a90ae543230cfe3
vivekYadav-asa <vy6751536@gmail.com>
2025-04-09 14:15:53 +0530

    changes

--- week6/index.js
+++ week6/index.js
@@ -1,21 +1,22 @@
 // authentication 
 const express = require('express')
+const JWT_SECRET="randomharkiratiLove"
 const app=express();
 app.use(express.json())
 const users=[];
-console.log(users);
+// console.log(users);
 
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
-         'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// function generateToken() {
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
+//          'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
+//     let token = "";
+//     for (let i = 0; i < 32; i++) {
+//         // use a simple function here
+//         token += options[Math.floor(Math.random() * options.length)];
+//     }
+//     return token;
+// }
 app.post('/signup',function(req,res){
 const username=req.body.username;
 const password=req.body.password;
@@ -35,30 +36,33 @@ res.json({
 app.post("/signin", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    /*
-  m2 for finding the user is same or not to whom we have to return a token 
+    
+//   m2 for finding the user is same or not to whom we have to return a token 
   let userFound=null;
   for(let i=0; i<users.length;i++){
   if(users[i].userName==userName && users[i].password==password){
   userFound=users[i];
   }
   }
-     */
+     
 
-    const user = users.find(function(u){
-        if(u.username==username&&u.password==password){
-            return true;
-        }
-        else{
-            return false;
-        }
-    });
+    // const user = users.find(function(u){
+    //     if(u.username==username&&u.password==password){
+    //         return true;
+    //     }
+    //     else{
+    //         return false;
+    //     }
+    // });
 
-    if (user) {
-        const token = generateToken();
-        user.token = token;//this line of code push the token in the global variable users
+    if (userFound) {
+        // const token = generateToken();
+        // user.token = token;//this line of code push the token in the global variable users
+        const token =jwt.sign({
+            username:username
+        },JWT_SECRET)
         res.send({
-            token
+            token:token
         })
         // console.log(users);
     } else {
@@ -71,10 +75,11 @@ app.post("/signin", (req, res) => {
 });
 app.get('/me',function(req,res){
 const token=req.headers.token;
-
+const decodedInformation=jwt.verify(token,JWT_SECRET);//{username:"harkiart"}
+const username=decodedInformation.username;
 let Founduser=null;
 for(let i=0; i<users.length;i++){
-    if(users[i].token==token){
+    if(users[i].username==username){
         Founduser=user[i];
     }
 }
@@ -91,4 +96,4 @@ else{
 }
 })
 
-app.listen(3003);
+app.listen(3004);
