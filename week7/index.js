6985556364fc04cc862c5f7f6ae43f73976da21f
vivekYadav-asa <vy6751536@gmail.com>
2025-05-05 23:34:19 +0530

    auth middleware

--- week7/index.js
+++ week7/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { UserModel,TodoModel}=require("./db")
+const jwt=require("jsonwebtoken");
+const JWT_SECRET="secret"
 const app = express();
 app.use(express.json());
 
@@ -19,18 +21,57 @@ app.post("/signup", async function(req, res) {
 });
 
 
-app.post("/signin", function(req, res) {
-
+app.post("/signin",async function(req, res) {
+    const email =req.body.email;
+    const password=req.body.password;
+    const user=await UserModel.findOne({
+    email:email,
+    password:password    
+    })
+    console.log(user);
+    
+    if(user){
+        const token=jwt.sign({
+            id:user._id
+        });
+        res.json({
+           token:token 
+        })
+    } 
+    else{
+        res.status(403).json({
+            message:"incorrect token "
+        })
+    }
 });
 
 
-app.post("/todo", function(req, res) {
-
+app.post("/todo", auth, function(req, res) {
+const userId=req.userId;
+res.json({
+    userId:userId
+})
 });
 
 
-app.get("/todos", function(req, res) {
-
+app.get("/todos", auth, function(req, res) {
+    const userId=req.userId;
+    res.json({
+        userId:userId
+    })
 });
+function auth(req,res,next){
+const token =req.header.token;
+const decodedData=jwt.verify(token.JWT_SECRET)
+}
+if(decodedData){
+    req.userId=decodedData.userId;
+    next();
+}
+else{
+    res.status(403).json({
+        message:"incorrect credentials"
+    })
+}
 
 app.listen(3000);
