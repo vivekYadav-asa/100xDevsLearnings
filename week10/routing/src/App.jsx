43c9800b5d5c8c3cb98f9c6fb856430604dd09e0
vivekYadav-asa <vy6751536@gmail.com>
2025-05-13 18:15:07 +0530

    learning about ref

--- week10/routing/src/App.jsx
+++ week10/routing/src/App.jsx
@@ -1,31 +1,51 @@
-import{BrowserRouter,Routes,Route} from "react-router-dom";
-function App(){
-return <div>
-    Allen | class-11 |class-12
-<BrowserRouter>
-<Routes>
-    <Route path="/neet/online-coaching-class11" element={<Class11Prog/>}/>
-    <Route path="/neet/online-coaching-class12" element={<Class12Prog/>}/>
-        <Route path="/" element={<Landing/>}/>
+// import{BrowserRouter,Routes,Route,Link} from "react-router-dom";
+// function App(){
+// return <div>
+
+import { useRef, useState } from "react"
+
+// <BrowserRouter>
+// <Link to="/"> ALLEN </Link>
+// <Link to="/neet/online-coaching-class11"> class11 </Link>
+// <Link to="/neet/online-coaching-class12"> class12 </Link>
+// <Routes>
+//     <Route path="/neet/online-coaching-class11" element={<Class11Prog/>}/>
+//     <Route path="/neet/online-coaching-class12" element={<Class12Prog/>}/>
+//         <Route path="/" element={<Landing/>}/>
     
-</Routes>
-</BrowserRouter>
-</div>
-function Class11Prog(){
+// </Routes>
+// </BrowserRouter>
+
+// </div>
+// function Class11Prog(){
+//     return <div>
+//         Class 11 coaching for neet
+//     </div>
+// }
+// function Landing(){
+//     return <div>
+//         Welcome to Allen
+//     </div>
+// }
+// function Class12Prog(){
+//     return <div>
+//         Class 12 coaching for neet
+//     </div>  
+// }
+// }
+// import{useRef} from 'react'
+function App(){
+    const inputRef=useRef()
+    function FocusOn(){
+        // document.getElementById("name").focus()
+inputRef.current.focus()
+    }
     return <div>
-        Class 11 coaching for neet
+        SignUP<br />
+    <input ref={inputRef} type={"text"} />
+    <input type={"text"} />
+    <button onClick={FocusOn}>sumbit</button>
     </div>
-}
-function Landing(){
-    return <div>
-        Welcome to Allen
-    </div>
-}
-function Class12Prog(){
-    return <div>
-        Class 12 coaching for neet
-    </div>  
-}
 }
 
 export default App
